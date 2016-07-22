292dc831ed37470165e3526824e1d3d3a9056db9
Yilun Huang <alanhuang.chn@gmail.com>
2016-07-22 17:22:04 +0900

    create new file

---
+++ NN_3layer.py
@@ -0,0 +1,35 @@
+'''
+三层的神经网络
+
+
+
+
+Created on 2016/06/03
+@author: Yilun Huang
+'''
+
+
+
+def bp():
+
+
+def fp(arg):
+
+
+
+def hypo(nn_params,layer_size,X,lambda):
+    #hypothesis, the function to predict the finall answer
+    '''
+    nn_params: unrolled parameter of this neural network
+    layer_size: a list containing each layer's size, including the input layer_size
+    X: the input examples
+    lambda: regularization parameter, deciding the penalty of parameters (weights)
+    '''
+
+    #reshape the nn_params
+
+    #compute the finall answer
+
+
+
+if __name__ = '__main__':
